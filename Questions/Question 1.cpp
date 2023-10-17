07cf2fcc5c41c441a4cdb98d5f11ef2c4f079dc7
JWRoosky <124602630+JWRoosky@users.noreply.github.com>
2023-10-17 10:30:47 -0600

    Update Question 1.cpp

--- Questions/Question 1.cpp
+++ Questions/Question 1.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 // a - z === 97 - 122
 char shiftChar(char c, int shift){
-    char result;
-    if (!isupper(c) && isalpha(c)){
+    char result; // Variable to contain shifted character
+    if (!isupper(c) && isalpha(c)){ // Ensures c is not uppercase and is an alphabetic character
         result = char(int(c + ((shift % 26) +26) - 97) % 26 + 97);
     }
     else{
