074bcd403ab84dfb41a81696068c12d77805a8b7
JWRoosky <124602630+JWRoosky@users.noreply.github.com>
2023-10-17 10:35:18 -0600

    Update Question 1.cpp

--- Questions/Question 1.cpp
+++ Questions/Question 1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 char shiftChar(char c, int shift){
     char result; // Variable to contain shifted character
     if (!isupper(c) && isalpha(c)){ // Ensures c is not uppercase and is an alphabetic character
-        result = char(int(c + ((shift % 26) +26) - 97) % 26 + 97);
+        result = char(int(c + ((shift % 26) + 26) - 97) % 26 + 97);
     }
     else{
         result = c;
@@ -15,3 +15,8 @@ char shiftChar(char c, int shift){
 
     return result;
 }
+
+
+// Line 10 ensures the shift value is within the valid alphabet range (shift % 26), adds 26 to ensure the shift value is positive, subtracts 97 to ensure
+// that 'a' (ASCII 97) is the zero reference point. It is modded by 26 again to ensure it is within valid alphabet range, and then 97 is added to bring it 
+// back to the lowercase ASCII values 97 - 122, and then finally casted to char.
